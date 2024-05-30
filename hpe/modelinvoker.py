b6a570ef80dfab9d935b5c2a622901034f204a10
Deekshith G L <117536596+DeekshithGL@users.noreply.github.com>
2024-05-30 23:17:13 +0530

    Add files via upload

--- hpe/modelinvoker.py
+++ hpe/modelinvoker.py
@@ -1,24 +1,24 @@
+# modelinvoker.py
+
 from .torch_serve import TorchServeManager
 from .vllm import VLLMManager
 
-
 class ModelInvoker:
-    def __init__(self, servingengine):
-        self.serving_engine = servingengine
-        self.manager = None
+    def __init__(self, serving_engine):
+        self.serving_engine = serving_engine
+        self.manager = self._get_manager(serving_engine)
+
+    def _get_manager(self, serving_engine):
+        if serving_engine.lower() == 'torchserve':
+            return TorchServeManager()
+        elif serving_engine.lower() == 'vllm':
+            return VLLMManager()
+        else:
+            raise ValueError(f"Unsupported serving engine: {serving_engine}")
 
     def invoke(self, model_name, model_file):
-        if self.serving_engine == 'torchserve':
-            self.manager = TorchServeManager()
-            self.invoke_torchserve(model_name, model_file)
-        elif self.serving_engine == 'vLLM':
-            self.manager = VLLMManager()
-            self.invoke_vLLM(model_name, model_file)
-        else:
-            raise ValueError(f"Invalid tunable: {self.serving_engine}")
+        self.manager.start(model_name, model_file)
 
-    def invoke_torchserve(self, model_name, model_file):
-        self.manager.start_torchserve(model_name, model_file)
+    def stop(self):
+        self.manager.stop()
 
-    def invoke_vLLM(self, model_name, model_file):
-        self.manager.start_vllm(model_name, model_file)
--- hpe/torch_serve.py
+++ hpe/torch_serve.py
@@ -1,15 +1,11 @@
+# torch_serve.py
+
 import os
 import subprocess
 import time
-
 import requests
 
 MODEL_STORE = "model_store"
-MODEL_FILES = {
-    "resnet18": "resnet-18.mar",
-    "bert": "bert.mar"
-}
-
 
 class TorchServeManager:
     def __init__(self):
@@ -17,44 +13,40 @@ class TorchServeManager:
 
     def is_torchserve_running(self):
         try:
-            ts_addr = "http://localhost:8080/ping"
-            response = requests.get(ts_addr)
-            if response.status_code == 200:
-                return True
-            else:
-                return False
+            response = requests.get("http://localhost:8080/ping")
+            return response.status_code == 200
         except requests.exceptions.ConnectionError:
             return False
 
-    def start_torchserve(self, model_name, model_file):
+    def start(self, model_name, model_file):
         if self.is_torchserve_running():
-            print("TorchServe is already running")
-            return
+            if self.current_model == model_name:
+                print("TorchServe already running with the selected model")
+                return
+            else:
+                self.stop()
 
         try:
-            print("Starting TorchServe...")
-            os.system(
-                f"torchserve --start --ncs --model-store {MODEL_STORE} --models {model_name.lower()}={model_file}")
-            # subprocess.run(["torchserve", "--start", "--ncs",
-            #                "--model-store", model_path])
+            print(f"Starting TorchServe with {model_name}...")
+            os.system(f"torchserve --start --ncs --model-store {MODEL_STORE} --models {model_name.lower()}={model_file}")
             self.current_model = model_name
             time.sleep(5)
         except Exception as e:
             print(f"Failed to start TorchServe: {e}")
 
         if self.is_torchserve_running():
-            print("TorchServe started successfully")
+            print(f"TorchServe started successfully with {model_name}")
         else:
-            print("TorchServe failed to start")
+            print(f"TorchServe failed to start with {model_name}")
 
-    def stop_torchserve(self):
+    def stop(self):
         if not self.is_torchserve_running():
             print("TorchServe is not running")
             return
 
         try:
             print("Stopping TorchServe...")
-            subprocess.run(["torchserve", "--stop"])
+            os.system("torchserve --stop")
             self.current_model = None
             time.sleep(5)
         except Exception as e:
